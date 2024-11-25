805a77f79e5b22476dcf716c79f66cf34229eff4
JustinLai <justinlth3@gmail.com>
2024-11-25 23:06:14 +0000

    Try Fix project link

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -32,20 +32,14 @@ export default function RootLayout({
         <div
             className="z-50 flex sticky top-0 h-12 flex-row text-pink-300 items-center border-b-2 backdrop-brightness-0">
             <Link href="/" className="p-10 text-2xl font-bold ease-in-out">Tsz Him Lai</Link>
-            <Link  href={{
-                pathname: '/',
-                hash: 'Projects',
-            }} className={"ease-in-out"}>Projects</Link>
+            <Link href="//#Projects" className={"ease-in-out"}>Projects</Link>
         </div>
         {children}
         <div className="h-[200px]"/>
         <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center pb-5 pt-4">
             <Link
                 className="flex items-center gap-2 hover:scale-110 hover:border-cyan-100 border-[1px] rounded-2xl p-1 transition"
-                href={{
-                    pathname: '/',
-                    hash: 'Projects',
-                }}
+                href="//#Projects"
                 rel="noopener noreferrer"
             >
                 Projects
