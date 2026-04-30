838ee8a7ae0b4e719fad39dbea972a8b60236678
JustinLai <justinlth3@gmail.com>
2026-04-30 17:28:58 +0100

    Add demo in the same page

--- src/app/Punctuation/page.tsx
+++ src/app/Punctuation/page.tsx
@@ -1,7 +1,20 @@
-﻿import Image from "next/image";
+﻿"use client"
+import Image from "next/image";
 import Link from "next/link";
+import {useState} from "react";
 
 export default function Page() {
+
+    const [game, playGame] = useState(
+        <button onClick={() =>
+            playGame(
+                <iframe
+                    width="400" height="690"
+                    src={"PunctuationWeb/index.html"}
+                />)}>
+            Play Game
+        </button>
+    );
     return (
         <div className="flex flex-col items-center min-h-screen">
             <div className=" w-[400px] lg:w-[800px] flex flex-col items-center">
@@ -20,6 +33,11 @@ export default function Page() {
                           className="text-cyan-200">GitHub</Link>
                     <p className="px-5">Unity</p>
                     <Link href={"./Punctuation/Demo/"} className="text-cyan-200">Demo</Link>
+                </div>
+                <div className="rounded-lg scale-100">
+                    <div className={"w-[400px] h-[690px] border-[1px] m-10 items-center flex justify-center"}>
+                        {game}
+                    </div>
                 </div>
                 <div className="w-full pt-5">
                     <h2 className="w-full">Description</h2>
